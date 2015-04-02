6e105e1ccd7a6d76009ec030d82cfa72d2e559d3
leiyuncun <546138263@qq.com>
2015-04-02 18:03:06 +0800

    fix alarmView UI and add image

--- Programming/Nevo/app/src/main/java/com/nevowatch/nevo/Fragment/AlarmFragment.java
+++ Programming/Nevo/app/src/main/java/com/nevowatch/nevo/Fragment/AlarmFragment.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.nevowatch.nevo.R;
@@ -22,6 +24,8 @@ public class AlarmFragment extends Fragment implements View.OnClickListener{
     private TextView mClockTextView;
     private String mClockSr;
     private ImageButton mImageButton;
+    private Button onButton;
+    private Button offButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,6 +37,10 @@ public class AlarmFragment extends Fragment implements View.OnClickListener{
         mClockTextView.setOnClickListener(this);
         mImageButton = (ImageButton) rootView.findViewById(R.id.edit_clock_imageButton);
         mImageButton.setOnClickListener(this);
+        onButton =  (Button)rootView.findViewById(R.id.on_mode_button);
+        onButton.setOnClickListener(this);
+        offButton =  (Button)rootView.findViewById(R.id.off_mode_button);
+        offButton.setOnClickListener(this);
 
     /*    if(savedInstanceState != null){
             mCallbacks.setClockTime(mClockSr);
@@ -72,6 +80,20 @@ public class AlarmFragment extends Fragment implements View.OnClickListener{
             case R.id.edit_clock_imageButton:
             case R.id.clock_textView:
                 mCallbacks.showTime();
+                break;
+            case R.id.on_mode_button:
+                Log.d("onButton","on");
+                offButton.setTextColor(0xff000000);
+                onButton.setTextColor(0xffffffff);
+                offButton.setSelected(false);
+                onButton.setSelected(true);
+                break;
+            case R.id.off_mode_button:
+                Log.d("offButton","off");
+                offButton.setTextColor(0xffffffff);
+                onButton.setTextColor(0xff000000);
+                offButton.setSelected(true);
+                onButton.setSelected(false);
                 break;
             default:
                 break;
