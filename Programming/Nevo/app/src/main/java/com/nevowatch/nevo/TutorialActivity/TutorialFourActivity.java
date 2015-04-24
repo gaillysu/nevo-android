db560fccec873a64fdd19656891ccc16147e594b
supernova <gaillysu@139.com>
2015-04-24 20:05:50 +0800

    remove code

--- Programming/Nevo/app/src/main/java/com/nevowatch/nevo/TutorialActivity/TutorialFourActivity.java
+++ Programming/Nevo/app/src/main/java/com/nevowatch/nevo/TutorialActivity/TutorialFourActivity.java
@@ -119,11 +119,6 @@ public class TutorialFourActivity extends Activity implements View.OnClickListen
 
     @Override
     public void connectionStateChanged(boolean isConnected) {
-        if(isConnected)
-        {
-            mFinishButton.setVisibility(View.VISIBLE);
-            mConnectImg.setImageResource(R.drawable.success);
-            mConnectButton.setVisibility(View.INVISIBLE);
-        }
+        
     }
 }
