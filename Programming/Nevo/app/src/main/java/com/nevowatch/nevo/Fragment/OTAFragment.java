753f3885cc334da5120677685772c02e406d6954
leiyuncun <546138263@qq.com>
2015-06-04 17:51:54 +0800

    fix OTAFragment UI

--- Programming/Nevo/app/src/main/java/com/nevowatch/nevo/Fragment/OTAFragment.java
+++ Programming/Nevo/app/src/main/java/com/nevowatch/nevo/Fragment/OTAFragment.java
@@ -1,20 +1,27 @@
 package com.nevowatch.nevo.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.nevowatch.nevo.MainActivity;
 import com.nevowatch.nevo.Model.Notification.NotificationType;
 import com.nevowatch.nevo.R;
 import com.nevowatch.nevo.View.NotificationItem;
+import com.nevowatch.nevo.View.RoundProgressBar;
 import com.nevowatch.nevo.ble.controller.OnSyncControllerListener;
 import com.nevowatch.nevo.ble.controller.SyncController;
 import com.nevowatch.nevo.ble.model.packet.NevoPacket;
+import com.nevowatch.nevo.ble.model.request.NumberOfStepsGoal;
 import com.nevowatch.nevo.ble.notification.NevoNotificationListener;
 
 import java.util.ArrayList;
@@ -24,10 +31,17 @@ import java.util.List;
  * NotificationFragment
  */
 public class OTAFragment extends Fragment
-        implements OnSyncControllerListener{
+        implements View.OnClickListener,OnSyncControllerListener{
 
     public static final String OTAFRAGMENT = "OTAFragment";
     public static final int OTAPOSITION = 4;
+    private Handler mUiHandler = new Handler(Looper.getMainLooper());
+    private RoundProgressBar mOTAProgressBar;
+    private TextView mMCUVersionTextView;
+    private TextView mBleVersionTextView;
+    private TextView mOTAProgressValueTextView;
+    private Button mReButton;
+    private ImageView mWarningButton;
 
     private void initListView(){
 
@@ -43,11 +57,36 @@ public class OTAFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.ota_fragment, container, false);
         initListView();
+        mOTAProgressBar = (RoundProgressBar) rootView.findViewById(R.id.otaProgressBar);
 
+        mMCUVersionTextView = (TextView) rootView.findViewById(R.id.mcuVersionLabel);
+
+        mBleVersionTextView = (TextView) rootView.findViewById(R.id.bleVersionLabel);
+
+        mOTAProgressValueTextView = (TextView) rootView.findViewById(R.id.progressValue);
+
+        mReButton = (Button) rootView.findViewById(R.id.reUpgradebutton);
+        mReButton.setOnClickListener(this);
+
+        mWarningButton = (ImageView) rootView.findViewById(R.id.warningButton);
+        mReButton.setOnClickListener(this);
 
         return rootView;
     }
 
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.reUpgradebutton:
+
+                break;
+            case R.id.warningButton:
+
+                break;
+            default:
+                break;
+        }
+    }
     @Override
     public void onResume() {
         super.onResume();
@@ -64,5 +103,21 @@ public class OTAFragment extends Fragment
     @Override
     public void connectionStateChanged(boolean isConnected) {
         ((MainActivity)getActivity()).replaceFragment(isConnected?NotificationFragment.NOTIPOSITION:ConnectAnimationFragment.CONNECTPOSITION, isConnected?NotificationFragment.NOTIFICATIONFRAGMENT:ConnectAnimationFragment.CONNECTFRAGMENT);
+    }
+
+    /**
+    *Set the display Ble and MCU version number
+    */
+    private void setFirmwaresVersionText(final String mcuStr,final String bleStr){
+        mMCUVersionTextView.setText(mcuStr);
+        mBleVersionTextView.setText(bleStr);
+    }
+
+    /**
+    *Set the OTA upgrade progress
+    */
+    public void setOTAProgressBar(final int progress){
+        mOTAProgressBar.setProgress(progress);
+        mOTAProgressValueTextView.setText(progress+"%");
     }
 }
