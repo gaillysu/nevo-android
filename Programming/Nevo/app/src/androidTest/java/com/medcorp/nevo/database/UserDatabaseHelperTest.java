73b734f14efe543a1e11b4a4b4fd9b3eeebc380b
supernova <gaillysu@139.com>
2015-12-07 18:17:18 +0800

    add new test functions

--- Programming/Nevo/app/src/androidTest/java/com/medcorp/nevo/database/UserDatabaseHelperTest.java
+++ Programming/Nevo/app/src/androidTest/java/com/medcorp/nevo/database/UserDatabaseHelperTest.java
@@ -17,6 +17,9 @@ public class UserDatabaseHelperTest extends AndroidTestCase {
 
     private UserDatabaseHelper db;
     private User dummyUser;
+    private User addUser;
+    private User removeUser;
+    private User updateUser;
 
     @Override
     protected void setUp() throws Exception {
@@ -24,6 +27,9 @@ public class UserDatabaseHelperTest extends AndroidTestCase {
         db = new UserDatabaseHelper(getContext());
         dummyUser = new User("Karl","Chow", 1, 946728000000l, 20, 70, 180, 946728000000l,"");
 
+        addUser = new User("KarlAdd","Chow", 1, 946728000000l, 20, 70, 180, 946728000000l,"");
+        removeUser = new User("KarlRemove","Chow", 1, 946728000000l, 20, 70, 180, 946728000000l,"");
+        updateUser = new User("KarlUpdate","Chow", 1, 946728000000l, 20, 70, 180, 946728000000l,"");
     }
 
     @Override
@@ -63,6 +69,47 @@ public class UserDatabaseHelperTest extends AndroidTestCase {
         assertEquals(newJoe.getLastName(),updatedJoe.getLastName());
         Log.w("Karl", "remove user");
         assertEquals(true,db.remove(newJoe.getId(),null));
+    }
+
+    public void testAdd()
+    {
+        User thisUser1 = db.add(addUser);
+        assertEquals(true, thisUser1!=null);
+        assertEquals(true, thisUser1.getId()!= -1);
+        User thisUser2 = db.get(thisUser1.getId(),null);
+
+        assertEquals(thisUser1.getId(),thisUser2.getId());
+        assertEquals(thisUser1.getFirstName(),thisUser2.getFirstName());
+        assertEquals(thisUser1.getLastName(),thisUser2.getLastName());
+        assertEquals(thisUser1.getAge(),thisUser2.getAge());
+    }
+    public void testRemove()
+    {
+        User thisUser1 = db.add(removeUser);
+        assertEquals(true, thisUser1!=null);
+        assertEquals(true, thisUser1.getId()!= -1);
+        assertEquals(true, db.remove(thisUser1.getId(), null));
+        User thisUser2 = db.get(thisUser1.getId(),null);
+        assertEquals(true, thisUser2==null);
+    }
+
+    public void testUpdate()
+    {
+        int userID = 1;
+        User thisUser1 = db.get(userID,null);
+        if(thisUser1 == null) return;
+
+        updateUser.setId(thisUser1.getId());
+        assertEquals(true, db.update(updateUser));
+
+        User thisUser2 = db.get(updateUser.getId(),null);
+        assertEquals(true, thisUser2==null);
+
+        assertEquals(updateUser.getId(),thisUser2.getId());
+        assertEquals(updateUser.getFirstName(), thisUser2.getFirstName());
+        assertEquals(updateUser.getLastName(),thisUser2.getLastName());
+        assertEquals(updateUser.getAge(),thisUser2.getAge());
+
     }
 
 }
