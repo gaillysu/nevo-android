e2619fb98e970b0efafd03a23a2539b0e3a197dc
supernova <gaillysu@139.com>
2015-12-11 18:02:33 +0800

    fix  steps fragment for today/history layout

--- Programming/Nevo/app/src/main/java/com/medcorp/nevo/Fragment/StepsHistoryFragment.java
+++ Programming/Nevo/app/src/main/java/com/medcorp/nevo/Fragment/StepsHistoryFragment.java
@@ -6,18 +6,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.medcorp.nevo.R;
-
+import butterknife.Bind;
+import butterknife.ButterKnife;
 /**
  * Created by Karl on 12/10/15.
  */
 public class StepsHistoryFragment extends Fragment{
 
+    @Bind(R.id.fragment_steps_history_distance)
+    TextView distance;
+
+    @Bind(R.id.fragment_steps_history_steps)
+    TextView steps;
+
+    @Bind(R.id.fragment_steps_history_consume)
+    TextView calories;
+
+    @Bind(R.id.fragment_steps_history_walkingdistance)
+    TextView walkingdistance;
+
+    @Bind(R.id.fragment_steps_history_walkingduration)
+    TextView walkingduration;
+
+    @Bind(R.id.fragment_steps_history_walkingcalories)
+    TextView walkingcalories;
+
+    @Bind(R.id.fragment_steps_history_runningdistance)
+    TextView runningdistance;
+
+    @Bind(R.id.fragment_steps_history_runningduration)
+    TextView runningduration;
+
+    @Bind(R.id.fragment_steps_history_runningcalories)
+    TextView runningcalories;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_steps_history, container, false);
+        ButterKnife.bind(this, view);
         // TODO Gailly, implement Step history fragment. See design.
         return view;
     }
--- Programming/Nevo/app/src/main/java/com/medcorp/nevo/Fragment/StepsTodayFragment.java
+++ Programming/Nevo/app/src/main/java/com/medcorp/nevo/Fragment/StepsTodayFragment.java
@@ -6,19 +6,52 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.medcorp.nevo.R;
+import com.medcorp.nevo.view.RoundProgressBar;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 /**
  * Created by Karl on 12/10/15.
  */
 public class StepsTodayFragment  extends Fragment{
 
+    @Bind(R.id.fragment_steps_relative_layout_sync)
+    RelativeLayout syncRelativeLayout;
+
+    @Bind(R.id.roundProgressBar)
+    RoundProgressBar roundProgressBar;
+
+    @Bind(R.id.fragment_steps_today_goal)
+    TextView goal;
+
+    @Bind(R.id.fragment_steps_today_reach)
+    TextView goal_reach;
+
+    @Bind(R.id.fragment_steps_today_progress)
+    TextView goal_progress;
+
+    @Bind(R.id.fragment_steps_today_distance)
+    TextView distance;
+
+    @Bind(R.id.fragment_steps_today_dailysteps)
+    TextView dailysteps;
+
+    @Bind(R.id.fragment_steps_today_calories)
+    TextView calories;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_steps_today, container, false);
-        // TODO Gailly, implement Step today fragment. See design.
+        ButterKnife.bind(this, view);
+        //TODO start sync nevo, when sync done, refresh screen
         return view;
     }
+
 }
