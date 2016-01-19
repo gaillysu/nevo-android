fee5da5cee464b290d0c40d1ad17e441383c6c34
supernova <gaillysu@139.com>
2016-01-19 17:07:36 +0800

    [fixing][fix alarm edit UI] [fixing] [fixing goal edit UI]

--- Programming/Nevo/app/src/main/java/com/medcorp/nevo/Activity/EditGoalsActivity.java
+++ Programming/Nevo/app/src/main/java/com/medcorp/nevo/Activity/EditGoalsActivity.java
@@ -5,9 +5,20 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.medcorp.nevo.R;
 import com.medcorp.nevo.activity.base.BaseActivity;
+import com.medcorp.nevo.adapter.AlarmEditAdapter;
+import com.medcorp.nevo.adapter.PresetEditAdapter;
+import com.medcorp.nevo.model.Preset;
+import com.medcorp.nevo.view.ToastHelper;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -15,10 +26,15 @@ import butterknife.ButterKnife;
 /**
  * Created by gaillysu on 15/12/23.
  */
-public class EditGoalsActivity extends BaseActivity  {
+public class EditGoalsActivity extends BaseActivity implements AdapterView.OnItemClickListener {
 
     @Bind(R.id.main_toolbar)
     Toolbar toolbar;
+
+    @Bind(R.id.activity_goals_list_view)
+    ListView presetListView;
+
+    private Preset  preset;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,13 +46,10 @@ public class EditGoalsActivity extends BaseActivity  {
         actionBar.setDisplayHomeAsUpEnabled(true);
         setTitle("Edit Goals");
         Bundle bundle = getIntent().getExtras();
-        Fragment fragment = EditGoalsFragment.instantiate(EditGoalsActivity.this,EditGoalsFragment.class.getName(),bundle);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.activity_goals_fragment_layout, fragment)
-                .addToBackStack(EditAlarmFragment.class.getName())
-                .commit();
-
+        preset = getModel().getPresetById(bundle.getInt("Preset_ID"));
+        presetListView.setVisibility(View.VISIBLE);
+        presetListView.setOnItemClickListener(this);
+        presetListView.setAdapter(new PresetEditAdapter(this,preset));
     }
 
     @Override
@@ -48,4 +61,71 @@ public class EditGoalsActivity extends BaseActivity  {
     public void onBackPressed() {
         finish();
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(position == 0)
+        {
+            new MaterialDialog.Builder(EditGoalsActivity.this)
+                    .title("Edit Goal")
+                    .content("input your goal.")
+                    .inputType(InputType.TYPE_CLASS_NUMBER)
+                    .input("", ""+preset.getSteps(), new MaterialDialog.InputCallback() {
+                        @Override
+                        public void onInput(MaterialDialog dialog, CharSequence input) {
+                            if(input.length()==0)return;
+                            preset.setSteps(Integer.parseInt(input.toString()));
+                            presetListView.setAdapter(new PresetEditAdapter(EditGoalsActivity.this, preset));
+                        }
+                    }).negativeText("Cancel").show();
+        }
+        else if(position == 1)
+        {
+            new MaterialDialog.Builder(EditGoalsActivity.this)
+                    .title("Edit Goal")
+                    .content("Label your goal.")
+                    .inputType(InputType.TYPE_CLASS_TEXT)
+                    .input("label name", preset.getLabel(), new MaterialDialog.InputCallback() {
+                        @Override
+                        public void onInput(MaterialDialog dialog, CharSequence input) {
+                            if(input.length()==0) return;
+                            preset.setLabel(input.toString());
+                            presetListView.setAdapter(new PresetEditAdapter(EditGoalsActivity.this, preset));
+                        }
+                    }).negativeText("Cancel")
+                    .show();
+        }
+        else if(position == 2)
+        {
+            if(!getModel().deleteAlarm(preset)){
+                ToastHelper.showShortToast(this, "Failed to delete goal");
+            }else{
+                ToastHelper.showShortToast(this, "Deleted goal!");
+            }
+            setResult(-1);
+            finish();
+        }
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.done_menu:
+                if(getModel().updatePreset(preset)){
+                    ToastHelper.showShortToast(EditGoalsActivity.this, "Saved Goal!");
+                    EditGoalsActivity.this.setResult(1);
+                    EditGoalsActivity.this.finish();
+                }else{
+                    ToastHelper.showShortToast(EditGoalsActivity.this,"Couldn't save the goal.!");
+                }
+                return true;
+            case android.R.id.home:
+                EditGoalsActivity.this.setResult(0);
+                EditGoalsActivity.this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
---
+++ Programming/Nevo/app/src/main/java/com/medcorp/nevo/adapter/PresetEditAdapter.java
@@ -0,0 +1,71 @@
+package com.medcorp.nevo.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.medcorp.nevo.R;
+import com.medcorp.nevo.model.Alarm;
+import com.medcorp.nevo.model.Preset;
+import com.medcorp.nevo.view.customfontview.RobotoTextView;
+
+/**
+ * Created by gaillysu on 16/1/19.
+ */
+public class PresetEditAdapter extends BaseAdapter {
+    Context context;
+    Preset preset;
+
+    public PresetEditAdapter(Context context, Preset preset)
+    {
+        super();
+        this.context = context;
+        this.preset = preset;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return preset;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.activity_preset_edit_list_view_item, parent, false);
+
+        RobotoTextView title = (RobotoTextView)itemView.findViewById(R.id.activity_preset_edit_list_view_item_title_label);
+        RobotoTextView summary = (RobotoTextView)itemView.findViewById(R.id.activity_preset_edit_list_view_item_summary_label);
+        if(position == 0)
+        {
+            title.setText(preset.getSteps()+"");
+            summary.setText("Set new goal");
+        }
+        else if(position == 1)
+        {
+            title.setText(preset.getLabel());
+            summary.setText("Set label for goal");
+        }
+        else if(position == 2)
+        {
+            title.setVisibility(View.GONE);
+            summary.setTextColor(Color.BLACK);
+            summary.setText("Delete goal");
+        }
+
+        return itemView;
+    }
+}
