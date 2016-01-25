928ec55e5d69e51de811c52ece92559deea4ecd3
supernova <gaillysu@139.com>
2016-01-25 12:08:31 +0800

    [fixing] [prompt  message to let user grant location access for Android 6 ]

--- Programming/Nevo/app/src/main/java/com/medcorp/nevo/Activity/tutorial/TutorialPage5Activity.java
+++ Programming/Nevo/app/src/main/java/com/medcorp/nevo/Activity/tutorial/TutorialPage5Activity.java
@@ -1,8 +1,14 @@
 package com.medcorp.nevo.activity.tutorial;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.WindowManager;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.medcorp.nevo.R;
 import com.medcorp.nevo.activity.base.BaseActivity;
 import com.medcorp.nevo.activity.observer.ActivityObservable;
@@ -28,6 +34,24 @@ public class TutorialPage5Activity extends BaseActivity implements ActivityObser
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_tutorial_page_5);
         ButterKnife.bind(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (ActivityCompat.checkSelfPermission(TutorialPage5Activity.this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                new MaterialDialog.Builder(this)
+                        .title(R.string.location_access_title)
+                        .content(R.string.location_access_content)
+                        .positiveText(getString(android.R.string.ok))
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(MaterialDialog dialog, DialogAction which) {
+                                ActivityCompat.requestPermissions(TutorialPage5Activity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                            }
+                        })
+                        .cancelable(false)
+                        .show();
+            }
+        }
+
     }
 
     @Override
