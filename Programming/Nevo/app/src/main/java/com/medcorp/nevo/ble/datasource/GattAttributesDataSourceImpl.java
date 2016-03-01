3989b76eb0f718b2865b4e0b937b34f0760a858e
supernova <gaillysu@139.com>
2016-03-01 12:35:44 +0800

    [fixing][fix Service and Callback characteristic UUID, add MEDService to app's  manifest file]

--- Programming/Nevo/app/src/main/java/com/medcorp/nevo/ble/datasource/GattAttributesDataSourceImpl.java
+++ Programming/Nevo/app/src/main/java/com/medcorp/nevo/ble/datasource/GattAttributesDataSourceImpl.java
@@ -26,56 +26,56 @@ public class GattAttributesDataSourceImpl implements GattAttributesDataSource{
 
     @Override
     public UUID getDeviceInfoBluetoothVersion() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.DEVICEINFO_FIRMWARE_VERSION));
     }
 
     @Override
     public UUID getDeviceInfoSoftwareVersion() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.DEVICEINFO_SOFTWARE_VERSION));
     }
 
     @Override
     public UUID getClientCharacteristicConfig() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.CLIENT_CHARACTERISTIC_CONFIG));
     }
 
     @Override
     public UUID getService() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.NEVO_SERVICE));
     }
 
     @Override
     public UUID getCallbackCharacteristic() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.NEVO_CALLBACK_CHARACTERISTIC));
     }
 
     @Override
     public UUID getInputCharacteristic() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.NEVO_INPUT_CHARACTERISTIC));
     }
 
     @Override
     public UUID getOtaCharacteristic() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.NEVO_OTA_CHARACTERISTIC));
     }
 
     @Override
     public UUID getNotificationCharacteristic() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.NEVO_NOTIFICATION_CHARACTERISTIC));
     }
 
     @Override
     public UUID getOtaService() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.NEVO_OTA_SERVICE));
     }
 
     @Override
     public UUID getOtaControlCharacteristic() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.NEVO_OTA_CONTROL_CHARACTERISTIC));
     }
 
     @Override
     public UUID getOtaCallbackCharacteristic() {
-        return UUID.fromString(context.getString(R.string.DEVICEINFO_UDID));
+        return UUID.fromString(context.getString(R.string.NEVO_OTA_CALLBACK_CHARACTERISTIC));
     }
 }
