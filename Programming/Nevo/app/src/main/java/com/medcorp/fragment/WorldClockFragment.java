d544bf23b4aa315787b0555b618b393dc184a078
jason <xiongcai@med-corp.net>
2016-11-23 15:48:35 +0800

    [change the world clock]

--- Programming/Nevo/app/src/main/java/com/medcorp/fragment/WorldClockFragment.java
+++ Programming/Nevo/app/src/main/java/com/medcorp/fragment/WorldClockFragment.java
@@ -1,14 +1,12 @@
 package com.medcorp.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
@@ -22,13 +20,12 @@ import net.medcorp.library.worldclock.City;
 
 import org.greenrobot.eventbus.EventBus;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.TimeZone;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
@@ -51,21 +48,11 @@ public class WorldClockFragment extends BaseObservableFragment {
     @Bind(R.id.world_clock_fragment_show_location_city_tv)
     TextView showLocationCityInfo;
 
-    //    other city info
-    @Bind(R.id.world_clock_fragment_show_other_city_tv)
-    TextView showOtherCityNameAndTime;
-    @Bind(R.id.show_other_city_sunrise_tv)
-    TextView showOtherSunrise;
-    @Bind(R.id.show_other_city_sunset_tv)
-    TextView showOtherCitySunset;
-    @Bind(R.id.show_other_city_info_ground)
-    LinearLayout showOtherCityGround;
-
     private Realm realm = Realm.getDefaultInstance();
     private RealmResults<City> cities;
-    private City city;
-    public static String FORMAT_LONG_TIME = "yyyy-MM-dd HH:mm:ss";
+    private City locationCity;
     private String otherCityName;
+    private TimeZone timeZone;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -89,45 +76,26 @@ public class WorldClockFragment extends BaseObservableFragment {
     }
 
     private void initView() {
-        Date date = new Date();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        String currentTime = format.format(date);
-        String[] currentTimeArray = currentTime.split("-");
-        showLocationDate.setText(currentTimeArray[2] + "," + new SimpleDateFormat("MMM").format(date) + "," + currentTimeArray[0]);
         otherCityName = Preferences.getSaveOtherCityName(WorldClockFragment.this.getContext());
-
         Calendar calendar = Calendar.getInstance();
-        TimeZone timeZone = calendar.getTimeZone();
-        String locationCityName = timeZone.getID().split("/")[1].replace("_", " ");
-        format = new SimpleDateFormat(FORMAT_LONG_TIME);
-        String[] localTimeStr = format.format(calendar.getTime()).split(" ");
-
-        if (new Integer(localTimeStr[1].split(":")[0]).intValue() <= 12) {
-            showLocationCityInfo.setText(getString(R.string.tell_user_this_location_time) + ": " + localTimeStr[1].split(":")[0] + ":" + localTimeStr[1].split(":")[1] + " AM");
-        } else {
-            showLocationCityInfo.setText(getString(R.string.tell_user_this_location_time) + ": " + localTimeStr[1].split(":")[0] + ":" + localTimeStr[1].split(":")[1] + " PM");
-        }
-        for (City city : cities) {
-            if (city.getName().equals(locationCityName)) {
-                this.city = city;
-            }
-        }
-
-        //set location sunrise sunset
-        setSunriseAndSunset(showSunriseTv, showSunsetTv, city, timeZone.getID(),true);
-
-        // set other city sunrise sunset
-        if (otherCityName != null) {
-            for (City city : cities) {
-                if ((city.getName() + ", " + city.getCountry()).equals(otherCityName)) {
-                    showOtherCityNameAndTime.setText(city.getName());
-                    Log.i("jason",city.toString());
-                    net.medcorp.library.worldclock.TimeZone zone = city.getTimezoneRef();
-                    setSunriseAndSunset(showOtherSunrise, showOtherCitySunset, city, zone.getName(),false);
+        showLocationDate.setText(calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.MONTH) + " ," + calendar.get(Calendar.YEAR));
+        if (otherCityName == null) {
+            otherCityName = timeZone.getID().split("/")[1].replace("_", " ");
+            showLocationCityInfo.setText(otherCityName);
+            timeZone = calendar.getTimeZone();
+            for(City city:cities){
+                if(city.getName().equals(otherCityName)){
+                    this.locationCity = city;
                 }
             }
+            setSunriseAndSunset(showSunriseTv, showSunsetTv,locationCity, timeZone.getID());
         } else {
-            showOtherCityGround.setVisibility(View.GONE);
+            for (City city : cities) {
+                if ((city.getName() + ", " + city.getCountry()).equals(otherCityName)) {
+                    showLocationCityInfo.setText(city.getName());
+                    setSunriseAndSunset(showSunriseTv, showSunsetTv, city, city.getTimezoneRef().getName());
+                }
+            }
         }
     }
 
@@ -148,25 +116,28 @@ public class WorldClockFragment extends BaseObservableFragment {
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.choose_other_city)
+    public void openEditWorldClock() {
+        startActivity(EditWorldClockActivity.class);
+    }
+
     //set sunrise and sunset value
-    public void setSunriseAndSunset(TextView sunrise, TextView sunset, City city, String zone,boolean isLocal) {
+    public void setSunriseAndSunset(TextView sunrise, TextView sunset, City city, String zone) {
 
         com.luckycatlabs.sunrisesunset.dto.Location sunriseLocation =
                 new com.luckycatlabs.sunrisesunset.dto.Location(city.getLat() + "", city.getLng() + "");
         SunriseSunsetCalculator calculator = new SunriseSunsetCalculator(sunriseLocation, zone);
         String officialSunrise = calculator.getOfficialSunriseForDate(Calendar.getInstance());
         String officialSunset = calculator.getOfficialSunsetForDate(Calendar.getInstance());
-        sunrise.setText(officialSunrise);
-        sunset.setText(officialSunset);
+        sunrise.setText(officialSunrise+" "+getString(R.string.time_able_morning));
+        sunset.setText(officialSunset+" "+getString(R.string.time_able_afternoon));
 
-        if(isLocal) {
-            byte sunriseHour = (byte) Integer.parseInt(officialSunrise.split(":")[0]);
-            byte sunriseMin = (byte) Integer.parseInt(officialSunrise.split(":")[1]);
-            byte sunsetHour = (byte) Integer.parseInt(officialSunset.split(":")[0]);
-            byte sunsetMin = (byte) Integer.parseInt(officialSunset.split(":")[1]);
-            byte timeZoneOffset = (byte) (Calendar.getInstance().getTimeZone().getRawOffset()/3600/1000);
-            EventBus.getDefault().post(new SunRiseAndSunSetWithZoneOffsetChangedEvent(timeZoneOffset, sunriseHour, sunriseMin, sunsetHour, sunsetMin));
-        }
+        byte sunriseHour = (byte) Integer.parseInt(officialSunrise.split(":")[0]);
+        byte sunriseMin = (byte) Integer.parseInt(officialSunrise.split(":")[1]);
+        byte sunsetHour = (byte) Integer.parseInt(officialSunset.split(":")[0]);
+        byte sunsetMin = (byte) Integer.parseInt(officialSunset.split(":")[1]);
+        byte timeZoneOffset = (byte) (Calendar.getInstance().getTimeZone().getRawOffset() / 3600 / 1000);
+        EventBus.getDefault().post(new SunRiseAndSunSetWithZoneOffsetChangedEvent(timeZoneOffset, sunriseHour, sunriseMin, sunsetHour, sunsetMin));
     }
 
     private void refreshClock() {
