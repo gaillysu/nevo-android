f83406b38d9cf213a862287275d9f3890109166b
DESKTOP-300QVBO\DengGang <xiongcai@med-corp.net>
2016-12-27 22:23:54 +0800

    [complete more setting set sync time]

--- Programming/Nevo/app/src/main/java/com/medcorp/activity/MoreSettingActivity.java
+++ Programming/Nevo/app/src/main/java/com/medcorp/activity/MoreSettingActivity.java
@@ -3,14 +3,14 @@ package com.medcorp.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import com.medcorp.R;
+import com.medcorp.adapter.MySpinnerAdapter;
 import com.medcorp.base.BaseActivity;
 import com.medcorp.util.Preferences;
 
@@ -30,14 +30,9 @@ public class MoreSettingActivity extends BaseActivity {
     @Bind(R.id.main_toolbar)
     Toolbar toolbar;
     @Bind(R.id.more_setting_select_unit_spinner)
-    Spinner selectUnitSpinner;
+    AppCompatSpinner selectUnitSpinner;
     @Bind(R.id.more_setting_select_sync_time_spinner)
-    Spinner selectPlaceSpinner;
-
-    private List<String> unitList;
-    private ArrayAdapter<String> unitAdapter;
-    private List<String> placeList;
-    private ArrayAdapter<String> placeAdapter;
+    AppCompatSpinner selectPlaceSpinner;
 
 
     @Override
@@ -54,20 +49,22 @@ public class MoreSettingActivity extends BaseActivity {
     }
 
     private void initData() {
-        unitList = new ArrayList<>();
-        placeList = new ArrayList<>();
+        List<String> unitList = new ArrayList<>();
+        List<String> placeList = new ArrayList<>();
 
         placeList.add(getString(R.string.more_setting_place_local));
         placeList.add(getString(R.string.more_setting_place_home));
         unitList.add(getString(R.string.user_select_metrics));
         unitList.add(getString(R.string.user_select_imperial));
 
-        unitAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, unitList);
-        placeAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,placeList);
-        placeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        MySpinnerAdapter placeAdapter = new MySpinnerAdapter(this, placeList);
+        MySpinnerAdapter unitAdapter = new MySpinnerAdapter(this, unitList);
         selectPlaceSpinner.setAdapter(placeAdapter);
         selectUnitSpinner.setAdapter(unitAdapter);
+
+        selectUnitSpinner.setSelection(Preferences.getUnitSelect(this) ? 0 : 1);
+        selectPlaceSpinner.setSelection(Preferences.getPlaceSelect(this) ? 0 : 1);
+
         selectUnitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -75,9 +72,9 @@ public class MoreSettingActivity extends BaseActivity {
                     Preferences.saveUnitSelect(MoreSettingActivity.this, false);
                 } else {
                     Preferences.saveUnitSelect(MoreSettingActivity.this, true);
-
                 }
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
@@ -86,9 +83,9 @@ public class MoreSettingActivity extends BaseActivity {
         selectPlaceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position == 0){
+                if (position == 0) {
                     Preferences.savePlaceSelect(MoreSettingActivity.this, false);
-                }else{
+                } else {
                     Preferences.savePlaceSelect(MoreSettingActivity.this, true);
                 }
 
